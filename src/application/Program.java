f06b3dcb456f1da072d83234f25449e6595619c5
Andreia de Moraes <monteiro.andreiam@gmail.com>
2022-05-22 18:53:16 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Lesson;
+import entities.Task;
+import entities.Video;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		List<Lesson> list = new ArrayList<>();
+		
+		System.out.print("Quantas aulas tem o curso? ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Dados da " + i +"a aula:");
+			System.out.print("Conteúdo ou tarefa (c/t)? ");
+			char lessonType = sc.next().charAt(0);
+			System.out.print("Título: ");
+			sc.nextLine();
+			String title = sc.nextLine();
+			
+			if (lessonType == 'c' || lessonType == 'C') {
+				System.out.print("URL do vídeo: ");
+				String url = sc.nextLine();
+				System.out.print("Duração em segundos: ");
+				int duration = sc.nextInt();
+				list.add(new Video(title, url, duration));
+			} else {
+				System.out.print("Descrição: ");
+				String description = sc.nextLine();
+				System.out.print("Quantidade de questões: ");
+				int questions = sc.nextInt();
+				list.add(new Task(title, description, questions));
+			}
+		}
+		
+		int sum = 0;
+		for (Lesson l : list) {
+			sum += l.duration();
+		}
+		
+		System.out.println();
+		System.out.printf("DURAÇÃO TOTAL DO CURSO = %d segundos", sum);
+		
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module plataforma_ensino {
+}
